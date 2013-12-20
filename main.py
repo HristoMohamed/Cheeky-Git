055334fc9cb1c58c21d4f0672718397721e93167
Hristo Mohamed <hristo.mohamed@gmail.com>
2013-12-20 17:07:34 +0200

    Added Class to see if the wifi card is OK to work with

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ from Daemon import Daemon
 
 
 
+class ValidWiFiMachine():
+
+    WIRELESS_SYS = "/sys/class/net/NETDEVICE/wireless"
+    WPA_SUPPLICANT = "/var/run/wpa_supplicant.pid"
+    has_interface = 0
+    has_wpa_supplicant = 0
+    installed_wireless_stack = 0
+    
+    def search_for_wireless_interface(self):
+        if os.path.exists(WIRELESS_SYS):
+            self.has_interface = 1  
+
+    def search_for_running_wpa_supplicant(self):
+        if os.path.exists(WPA_SUPPLICANT):
+            self.has_wpa_supplicant = 1
+
+         
+
 #TODO think of a better name dammit
 def main_function():
 	#DOES STUFF?
@@ -19,23 +37,23 @@ def main_function():
 
 
 
-class QuotaDaemon(Daemon):
+class CheekyGitDaemon(Daemon):
     def run(self):
         while True:
             #daemon starter funcheck_quota()
 
 if __name__ == "__main__":
     try:
-        daemon = QuotaDaemon('/var/run/cheeky_git.pid')
+        daemon = CheekyGitDaemon('/var/run/cheeky_git.pid')
     except:
         try:
-            daemon = QuotaDaemon('/run/cheeky_git.pid')
+            daemon = CheekyGitDaemon('/run/cheeky_git.pid')
         except:
             try:
-                daemon = QuotaDaemon('/var/spool/cheeky_git.pid')
+                daemon = CheekyGitDaemon('/var/spool/cheeky_git.pid')
             except:
                 print "Warning, creating daemon pid in /tmp/"
-                daemon = QuotaDaemon('/tmp/cheeky_git.pid')
+                daemon = CheekyGitDaemon('/tmp/cheeky_git.pid')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()
